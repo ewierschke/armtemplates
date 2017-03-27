0d3e910637afef1cab22687857e8171c587f27cc
Erik Wierschke <erik.wierschke@plus3it.com>
2017-03-27 16:41:05 -0400

    add joindomain

---
+++ scripts/joindomain.ps1
@@ -0,0 +1,15 @@
+param (
+    [Parameter(Mandatory=$True)]
+    [string]$domainnetbiosname,
+ 
+    [Parameter(Mandatory=$True)]
+    [string]$username,
+
+    [Parameter(Mandatory=$True)]
+    [Security.SecureString]$password
+)
+
+$secpassword = $password | ConvertTo-SecureString -asPlainText -Force
+$qualusername = "$domainnetbiosname\$username" 
+$credential = New-Object System.Management.Automation.PSCredential($qualusername,$secpassword)
+Add-Computer -DomainName $domainnetbiosname -Credential $credential
