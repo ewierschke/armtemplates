afe92fdca36b6b22ae4f6a22ad5e362fd3a8a555
Erik Wierschke <erik.wierschke@plus3it.com>
2017-03-29 16:57:50 -0400

    change to atstartup

--- scripts/joindomain.ps1
+++ scripts/joindomain.ps1
@@ -144,7 +144,7 @@ $msg = "Please upgrade Powershell and try again."
 $taskname = "RunNextScript"
 if ($PSVersionTable.psversion.major -ge 4) {
     $A = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass ${JoinDomainDir}\${nextscript}.ps1"
-    $T = New-ScheduledTaskTrigger -AtLogon
+    $T = New-ScheduledTaskTrigger -AtStartup
     $P = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -RunLevel "Highest" -LogonType "ServiceAccount"
     $S = New-ScheduledTaskSettingsSet
     $D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
