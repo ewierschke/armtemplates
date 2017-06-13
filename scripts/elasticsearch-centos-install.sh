f6eea6dfac37adb0dac8a564a774734f2e8f48af
Erik Wierschke <erik.wierschke@plus3it.com>
2017-06-13 15:49:28 -0400

    tmp fix

--- scripts/elasticsearch-centos-install.sh
+++ scripts/elasticsearch-centos-install.sh
@@ -548,6 +548,7 @@ firewall-cmd --zone=public --add-port=9305/tcp
 chmod 777 /root/update.sh
 yum -y install at
 yum -y install epel-release && yum -y --enablerepo=epel install python-pip wget && pip install --upgrade pip setuptools watchmaker && watchmaker -n --log-level debug --log-dir=/var/log/watchmaker
+sed -i_bak -e '/tmp/d' /etc/fstab
 at now + 2 minutes -f /root/update.sh
 
 exit 0
