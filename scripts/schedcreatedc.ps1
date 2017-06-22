1de4cc2d6b0505fb4feb7ac2373351d3a93c7068
Erik Wierschke <erik.wierschke@plus3it.com>
2017-06-22 09:32:27 -0400

    scheduled task string cleanup

--- scripts/schedcreatedc.ps1
+++ scripts/schedcreatedc.ps1
@@ -166,7 +166,7 @@ $msg = "Please upgrade Powershell and try again."
 
 $taskname = "RunNextScript"
 if ($PSVersionTable.psversion.major -ge 4) {
-    $A = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command "& '${schedcreatedcDir}\${nextscript}.ps1' '${Username}' '${adminpass}' '${ADRestoreModeadminpass}' '${Domain}' 'Replica DC'""
+    $A = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-command & '${schedcreatedcDir}\${nextscript}.ps1' '${Username}' '${adminpass}' '${ADRestoreModeadminpass}' '${Domain}' 'Replica DC'"
     $T = New-ScheduledTaskTrigger -AtStartup
     $P = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -RunLevel "Highest" -LogonType "ServiceAccount"
     $S = New-ScheduledTaskSettingsSet
