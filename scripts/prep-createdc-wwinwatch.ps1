eba44c8123458617f759813247cd41128fac98c3
Erik Wierschke <erik.wierschke@plus3it.com>
2017-06-23 09:15:11 -0400

    for repl dc

---
+++ scripts/prep-createdc-wwinwatch.ps1
@@ -0,0 +1,175 @@
+#Get Parameters
+param (
+    [Parameter(Mandatory=$True,ValueFromPipelineByPropertyName=$true)]
+    [String]$ADDomainAdminUsername,
+
+    [Parameter(Mandatory=$True,ValueFromPipelineByPropertyName=$true)]
+    [String]$ADDomainAdminPw,
+
+    [Parameter(Mandatory=$True,ValueFromPipelineByPropertyName=$true)]
+    [String]$ADRestoreModePw,
+
+    [Parameter(Mandatory=$True,ValueFromPipelineByPropertyName=$true)]
+    [String]$ADDomainDNSName,
+
+    [Parameter(Mandatory=$True,ValueFromPipelineByPropertyName=$true)]
+    [ValidateSet("Primary DC","Replica DC")]
+    [string]$DcRole
+)
+
+# Define System variables
+$prepcreatedcDir = "${env:SystemDrive}\buildscripts\0a-prepcreatedc"
+$prepcreatedcLogDir = "${prepcreatedcDir}\Logs"
+$LogSource = "prepcreatedc"
+$DateTime = $(get-date -format "yyyyMMdd_HHmm_ss")
+$prepcreatedcLogFile = "${prepcreatedcLogDir}\prepcreatedc-log_${DateTime}.txt"
+$ScriptName = $MyInvocation.mycommand.name
+$ErrorActionPreference = "Stop"
+$credspath = "${env:SystemDrive}\buildscripts"
+$nextscript = "schedcreatedc"
+
+# Define Functions
+function log {
+    [CmdLetBinding()]
+    Param(
+        [Parameter(Mandatory=$false,Position=0,ValueFromPipeLine=$true)] [string[]]
+        $LogMessage,
+        [Parameter(Mandatory=$false,Position=1)] [string]
+        $EntryType="Information",
+        [Parameter(Mandatory=$false,Position=2)] [string]
+        $LogTag="${ScriptName}"
+    )
+    PROCESS {
+        foreach ($message in $LogMessage) {
+            $date = get-date -format "yyyyMMdd.HHmm.ss"
+            Manage-Output -EntryType $EntryType "${date}: ${LogTag}: $message"
+        }
+    }
+}
+
+function die($Msg) {
+    log -EntryType "Error" -LogMessage $Msg; Stop-Transcript; throw
+}
+
+function Manage-Output {
+    [CmdLetBinding()]
+    Param(
+        [Parameter(Mandatory=$false,Position=0,ValueFromPipeLine=$true)] [string[]]
+        $Output,
+        [Parameter(Mandatory=$false,Position=1)] [string]
+        $EntryType="Information"
+    )
+    PROCESS {
+        foreach ($str in $Output) {
+            #Write to the event log
+            Write-EventLog -LogName Application -Source "${LogSource}" -EventId 1 -EntryType $EntryType -Message "${str}"
+            #Write to the default stream (this way we don't clobber the output stream, and the output will be captured by Start-Transcript)
+            "${str}" | Out-Default
+        }
+    }
+}
+
+function Set-RegistryValue($Key,$Name,$Value,$Type=[Microsoft.win32.registryvaluekind]::DWord) {
+    $Parent=split-path $Key -parent
+    $Parent=get-item $Parent
+    $Key=get-item $Key
+    $Keyh=$Parent.opensubkey($Key.name.split("\")[-1],$true)
+    $Keyh.setvalue($Name,$Value,$Type)
+    $Keyh.close()
+}
+
+function Set-OutputBuffer($Width=10000) {
+    $keys=("hkcu:\console\%SystemRoot%_System32_WindowsPowerShell_v1.0_powershell.exe",
+           "hkcu:\console\%SystemRoot%_SysWOW64_WindowsPowerShell_v1.0_powershell.exe")
+    # other titles are ignored
+    foreach ($key in $keys) {
+        md $key -verbose -force
+        Set-RegistryValue $key FontSize 0x00050000
+        Set-RegistryValue $key ScreenBufferSize 0x02000200
+        Set-RegistryValue $key WindowSize 0x00200200
+        Set-RegistryValue $key FontFamily 0x00000036
+        Set-RegistryValue $key FontWeight 0x00000190
+        Set-ItemProperty $key FaceName "Lucida Console"
+
+        $bufferSize=$host.ui.rawui.bufferSize
+        $bufferSize.width=$Width
+        $host.ui.rawui.BufferSize=$BufferSize
+        $maxSize=$host.ui.rawui.MaxWindowSize
+        $windowSize=$host.ui.rawui.WindowSize
+        $windowSize.width=$maxSize.width
+        $host.ui.rawui.WindowSize=$windowSize
+    }
+}
+
+#SCRIPT
+# Create the prepcreatedc log directory
+New-Item -Path $prepcreatedcDir -ItemType "directory" -Force 2>&1 > $null
+New-Item -Path $prepcreatedcLogDir -ItemType "directory" -Force 2>&1 > $null
+# Increase the screen width to avoid line wraps in the log file
+Set-OutputBuffer -Width 10000
+# Start a transcript to record script output
+Start-Transcript $prepcreatedcLogFile
+
+# Create a "prepcreatedc" event log source
+try {
+    New-EventLog -LogName Application -Source "${LogSource}"
+} catch {
+    if ($_.Exception.GetType().FullName -eq "System.InvalidOperationException") {
+        # Event log already exists, log a message but don't force an exit
+        log "Event log source, ${LogSource}, already exists. Continuing..."
+    } else {
+        # Unhandled exception, log an error and exit!
+        "$(get-date -format "yyyyMMdd.HHmm.ss"): ${ScriptName}: ERROR: Encountered a problem creating the event log source." | Out-Default
+        Stop-Transcript
+        throw
+    }
+}
+
+# Get the next script
+log -LogTag ${ScriptName} "Downloading ${nextscript}.ps1"
+Invoke-Webrequest "https://raw.githubusercontent.com/ewierschke/armtemplates/runwincustdata/scripts/${nextscript}.ps1" -Outfile "${prepcreatedcDir}\${nextscript}.ps1";
+
+# Write Creds to disk
+log -LogTag ${ScriptName} "writing creds"
+$LclAdminNameFilePath = "${credspath}\lcladminname.txt"
+$LclAdminCredsFilePath = "${credspath}\lcladminpass.txt"
+$LclAdminKeyFilePath = "${credspath}\lcladminkey.txt"
+Set-Content $LclAdminNameFilePath $ADDomainAdminUsername
+$LclAdminSecurePwd = $ADDomainAdminPw | ConvertTo-SecureString -AsPlainText -Force
+$LclAdminKey = New-Object Byte[] 32
+[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($LclAdminKey)
+Set-Content $LclAdminKeyFilePath $LclAdminKey
+$LclAdminPass = $LclAdminSecurePwd | ConvertFrom-SecureString -Key $LclAdminKey
+Add-Content $LclAdminCredsFilePath $LclAdminPass
+
+# Write Creds to disk
+log -LogTag ${ScriptName} "writing pass"
+$ADRestoreModeCredsFilePath = "${credspath}\adrestoremodepass.txt"
+$ADRestoreModeKeyFilePath = "${credspath}\adrestoremodekey.txt"
+$ADRestoreModeSecurePwd = $ADRestoreModePw | ConvertTo-SecureString -AsPlainText -Force
+$ADRestoreModeKey = New-Object Byte[] 32
+[Security.Cryptography.RNGCryptoServiceProvider]::Create().GetBytes($ADRestoreModeKey)
+Set-Content $ADRestoreModeKeyFilePath $ADRestoreModeKey
+$ADRestoreModePass = $ADRestoreModeSecurePwd | ConvertFrom-SecureString -Key $ADRestoreModeKey
+Add-Content $ADRestoreModeCredsFilePath $ADRestoreModePass
+
+$DomainnameFilePath = "${credspath}\domainname.txt"
+Set-Content $DomainnameFilePath $ADDomainDNSName
+
+#Create an atstartup scheduled task to run next script
+log -LogTag ${ScriptName} "Registering a scheduled task at startup to run the next script"
+$msg = "Please upgrade Powershell and try again."
+
+$taskname = "RunNextScript"
+if ($PSVersionTable.psversion.major -ge 4) {
+    $A = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -file ${prepcreatedcDir}\${nextscript}.ps1"
+    $T = New-ScheduledTaskTrigger -AtStartup
+    $P = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -RunLevel "Highest" -LogonType "ServiceAccount"
+    $S = New-ScheduledTaskSettingsSet
+    $D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
+    Register-ScheduledTask -TaskName $taskname -InputObject $D 2>&1 | log -LogTag ${ScriptName}
+} else {
+    invoke-expression "& $env:systemroot\system32\schtasks.exe /create /SC ONLOGON /RL HIGHEST /NP /V1 /RU SYSTEM /F /TR `"msg * /SERVER:%computername% ${msg}`" /TN `"${taskname}`"" 2>&1 | log -LogTag ${ScriptName}
+}
+
+#Reboot controlled by domain join extension
