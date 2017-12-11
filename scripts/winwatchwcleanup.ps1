e1cc69503567f97e5c6554bc59a69dab29ae02e8
Erik Wierschke <erik.wierschke@plus3it.com>
2017-12-11 11:24:26 -0500

    added patch installation

--- scripts/winwatchwcleanup.ps1
+++ scripts/winwatchwcleanup.ps1
@@ -9,6 +9,27 @@ param (
 
 # Remove previous scheduled task
 Unregister-ScheduledTask -TaskName "RunNextScript" -Confirm:$false;
+
+#Install Windows Updates
+try {
+    #Verify if PowerShellGet module is installed. If not install
+    if (!(Get-Module -Name PowerShellGet)){
+        Invoke-WebRequest 'https://download.microsoft.com/download/C/4/1/C41378D4-7F41-4BBE-9D0D-0E4F98585C61/PackageManagement_x64.msi' -OutFile $($env:temp +'\PackageManagement_x64.msi')
+        Start-Process $($env:temp +'\PackageManagement_x64.msi') -ArgumentList "/qn" -Wait
+    }
+    #Verify if PSWindowsUpdate PowerShell Module is installed. If not install.
+    if (!(Get-Module -Name PSWindowsUpdate -List)){
+        Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+        Install-Module -Name PSWindowsUpdate -Scope AllUsers -Confirm:$false -Force
+    }
+    #module updated need to test approach using new cmdlets
+    #Get-WUInstall -WindowsUpdate -AcceptAll -AutoReboot -Confirm:$FALSE -ErrorAction stop
+    Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -IgnoreReboot
+}
+catch {
+    Write-Output "Oops. Something failed"
+}
+
 #open IE to initialize cert
 $ie = new-object -com "InternetExplorer.Application"
 $ie.navigate("http://s3.amazonaws.com/app-chemistry/files/")
