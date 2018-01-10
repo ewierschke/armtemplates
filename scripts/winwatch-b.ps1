dbb4d350b28d7298f011176c4f0afee119879568
Erik Wierschke <erik.wierschke@plus3it.com>
2018-01-10 10:54:14 -0500

    from wam usage

--- scripts/winwatch-b.ps1
+++ scripts/winwatch-b.ps1
@@ -3,22 +3,18 @@ $PythonUrl = "https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe"
 $PypiUrl = "https://pypi.org/simple"
 
 # Download bootstrap file
-#$BootstrapFile = "${Env:Temp}\$(${BootstrapUrl}.split('/')[-1])"
-$BootstrapFile = "$env:windir\Temp\$(${BootstrapUrl}.split('/')[-1])"
+$BootstrapFile = "${Env:Temp}\$(${BootstrapUrl}.split('/')[-1])"
 (New-Object System.Net.WebClient).DownloadFile("$BootstrapUrl", "$BootstrapFile")
 
 # Install python
-#& "$BootstrapFile" -PythonUrl "$PythonUrl" -Verbose -ErrorAction Stop
 $params = "`"$BootstrapFile`" -PythonUrl `"$PythonUrl`" -Verbose -ErrorAction Stop"
 Start-Process powershell -Argument $params -NoNewWindow -Wait
-Get-ChildItem env: | Format-List | Out-File $env:windir\Temp\LocalSystemEnv1.log
-$env:Path = "$env:Path;C:\Program Files\Python36\Scripts\;C:\Program Files\Python36\"
-Get-ChildItem env: | Format-List | Out-File $env:windir\Temp\LocalSystemEnv3.log
+$env:Path = "$env:Path;$env:ProgramFiles\Python36\Scripts\;$env:ProgramFiles\Python36\"
 
 # Install watchmaker
-pip install --index-url="$PypiUrl" --upgrade pip setuptools boto3 --log $env:windir\Temp\pip1.log
-pip download --dest $env:windir\Temp watchmaker --log $env:windir\Temp\pip2.log
-pip install --no-index --find-links=$env:windir\Temp watchmaker --log $env:windir\Temp\pip3.log
+pip install --index-url="$PypiUrl" --upgrade pip setuptools boto3
+pip download --dest $env:windir\Temp watchmaker
+pip install --no-index --find-links=$env:windir\Temp watchmaker
 
 # Run watchmaker
 watchmaker --log-level debug --log-dir=C:\Watchmaker\Logs
