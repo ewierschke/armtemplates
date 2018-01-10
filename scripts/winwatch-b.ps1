77c7e093a1cb46e34f7c67c4627944775b2d7c26
Erik Wierschke <erik.wierschke@plus3it.com>
2018-01-10 13:47:08 -0500

    from wam usage

--- scripts/winwatch-b.ps1
+++ scripts/winwatch-b.ps1
@@ -1,3 +1,4 @@
+
 $BootstrapUrl = "https://raw.githubusercontent.com/plus3it/watchmaker/master/docs/files/bootstrap/watchmaker-bootstrap.ps1"
 $PythonUrl = "https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe"
 $PypiUrl = "https://pypi.org/simple"
@@ -12,9 +13,9 @@ Start-Process powershell -Argument $params -NoNewWindow -Wait
 $env:Path = "$env:Path;$env:ProgramFiles\Python36\Scripts\;$env:ProgramFiles\Python36\"
 
 # Install watchmaker
-pip install --index-url="$PypiUrl" --upgrade pip setuptools boto3
-pip download --dest $env:windir\Temp watchmaker
-pip install --no-index --find-links=$env:windir\Temp watchmaker
+pip install --index-url="$PypiUrl" --upgrade pip setuptools watchmaker
 
 # Run watchmaker
 watchmaker --log-level debug --log-dir=C:\Watchmaker\Logs
+
+
