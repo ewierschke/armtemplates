c43a19c94591cc41454669694b2202a4af9f2806
Erik Wierschke <erik.wierschke@plus3it.com>
2018-01-17 07:24:49 -0500

    uncomment path

--- scripts/winwatch-b.ps1
+++ scripts/winwatch-b.ps1
@@ -1,5 +1,5 @@
 
-$BootstrapUrl = "https://raw.githubusercontent.com/ewierschke/watchmaker/develop/docs/files/bootstrap/watchmaker-bootstrap.ps1"
+$BootstrapUrl = "https://raw.githubusercontent.com/ewierschke/watchmaker/bootstrap/docs/files/bootstrap/watchmaker-bootstrap.ps1"
 $PythonUrl = "https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe"
 $PypiUrl = "https://pypi.org/simple"
 
@@ -12,7 +12,7 @@ Get-ChildItem env: | Format-List | Out-File $env:windir\Temp\Beforebootstrap.log
 $params = "`"$BootstrapFile`" -PythonUrl `"$PythonUrl`" -Verbose -ErrorAction Stop"
 Start-Process powershell -Argument $params -NoNewWindow -Wait
 Get-ChildItem env: | Format-List | Out-File $env:windir\Temp\Afterbootstrap.log
-#$env:Path = "$env:Path;$env:ProgramFiles\Python36\Scripts\;$env:ProgramFiles\Python36\"
+$env:Path = "$env:Path;$env:ProgramFiles\Python36\Scripts\;$env:ProgramFiles\Python36\"
 
 # Install watchmaker
 pip install --index-url="$PypiUrl" --upgrade pip setuptools watchmaker
