3b53e49f405e8d94ccbec092d065c7c52985fe35
Erik Wierschke <erik.wierschke@plus3it.com>
2018-01-17 08:53:10 -0500

    match usage, point to custom bootstrap

--- scripts/winwatch.ps1
+++ scripts/winwatch.ps1
@@ -1,4 +1,4 @@
-$BootstrapUrl = "https://raw.githubusercontent.com/plus3it/watchmaker/master/docs/files/bootstrap/watchmaker-bootstrap.ps1"
+$BootstrapUrl = "https://raw.githubusercontent.com/ewierschke/watchmaker/bootstrap/docs/files/bootstrap/watchmaker-bootstrap.ps1"
 $PythonUrl = "https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe"
 $PypiUrl = "https://pypi.org/simple"
 
@@ -10,11 +10,7 @@ $BootstrapFile = "${Env:Temp}\$(${BootstrapUrl}.split('/')[-1])"
 & "$BootstrapFile" -PythonUrl "$PythonUrl" -Verbose -ErrorAction Stop
 
 # Install watchmaker
-$WinWatchDir = "${env:SystemDrive}\winwatch"
-New-Item -Path $WinWatchDir -ItemType "directory" -Force 2>&1 > $null
-cd $WinWatchDir
 pip install --index-url="$PypiUrl" --upgrade pip setuptools watchmaker
 
 # Run watchmaker
 watchmaker --log-level debug --log-dir=C:\Watchmaker\Logs
-exit $LASTEXITCODE
