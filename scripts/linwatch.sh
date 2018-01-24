4da7d5055baa437fc0f94b555b1152e6ab12b4f9
Erik Wierschke <erik.wierschke@plus3it.com>
2018-01-24 09:58:11 -0500

    add script to check and start firewalld before wam

--- scripts/linwatch.sh
+++ scripts/linwatch.sh
@@ -4,6 +4,13 @@
     printf "shutdown -r now\n"
 ) > /root/update.sh
 chmod 777 /root/update.sh
+service=firewalld
+if (( $(ps -ef | grep -v grep | grep $service | wc -l) > 0 ))
+then
+echo "$service is running!!!"
+else
+/bin/systemctl start $service
+fi
 yum -y install at
 yum -y install epel-release && yum -y --enablerepo=epel install python-pip wget && pip install --upgrade pip setuptools watchmaker && watchmaker -n --log-level debug --log-dir=/var/log/watchmaker
 at now + 2 minutes -f /root/update.sh
