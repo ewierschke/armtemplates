4224dfd145627fdbd4213a83b4074d205941b07c
Erik Wierschke <erik.wierschke@plus3it.com>
2018-02-23 07:58:59 -0500

    remove ie call and move task dereg to after wam call

--- scripts/winwatchwcleanup.ps1
+++ scripts/winwatchwcleanup.ps1
@@ -7,21 +7,14 @@ param (
     [String]$WatchmakerParam2
 )
 
-# Remove previous scheduled task
-$taskName = "RunNextScript";
-$taskExists = Get-ScheduledTask | Where-Object {$_.TaskName -like $taskName}
-if ($taskExists) {
-    Unregister-ScheduledTask -TaskName ${taskName} -Confirm:$false;
-}
-
 #Install Updates
 #. { iwr -useb http://boxstarter.org/bootstrapper.ps1 } | iex; get-boxstarter -Force
 #Enable-MicrosoftUpdate
 #Install-WindowsUpdate -SuppressReboots -AcceptEula
 
 #open IE to initialize cert
-$ie = new-object -com "InternetExplorer.Application"
-$ie.navigate("http://s3.amazonaws.com/app-chemistry/files/")
+#$ie = new-object -com "InternetExplorer.Application"
+#$ie.navigate("http://s3.amazonaws.com/app-chemistry/files/")
 
 $BootstrapUrl = "https://raw.githubusercontent.com/plus3it/watchmaker/master/docs/files/bootstrap/watchmaker-bootstrap.ps1"
 $PythonUrl = "https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe"
@@ -41,5 +34,12 @@ pip install --index-url="$PypiUrl" --upgrade pip setuptools watchmaker
 watchmaker --no-reboot --log-level debug --log-dir=C:\Watchmaker\Logs ${WatchmakerParam} ${WatchmakerParam2}
 
 gpupdate /force
+
+# Remove previous scheduled task
+$taskName = "RunNextScript";
+$taskExists = Get-ScheduledTask | Where-Object {$_.TaskName -like $taskName}
+if ($taskExists) {
+    Unregister-ScheduledTask -TaskName ${taskName} -Confirm:$false;
+}
 
 powershell.exe "Restart-Computer -Force -Verbose";
