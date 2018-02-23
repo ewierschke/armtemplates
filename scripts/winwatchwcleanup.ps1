ce506fba84321ecdba53243112d1ec98e64f7a87
Erik Wierschke <erik.wierschke@plus3it.com>
2018-02-23 10:10:01 -0500

    new py ver and point to my bootstrap

--- scripts/winwatchwcleanup.ps1
+++ scripts/winwatchwcleanup.ps1
@@ -16,8 +16,8 @@ param (
 #$ie = new-object -com "InternetExplorer.Application"
 #$ie.navigate("http://s3.amazonaws.com/app-chemistry/files/")
 
-$BootstrapUrl = "https://raw.githubusercontent.com/plus3it/watchmaker/master/docs/files/bootstrap/watchmaker-bootstrap.ps1"
-$PythonUrl = "https://www.python.org/ftp/python/3.6.3/python-3.6.3-amd64.exe"
+$BootstrapUrl = "https://raw.githubusercontent.com/ewierschke/watchmaker/bootstrap/docs/files/bootstrap/watchmaker-bootstrap.ps1"
+$PythonUrl = "https://www.python.org/ftp/python/3.6.4/python-3.6.4-amd64.exe"
 $PypiUrl = "https://pypi.org/simple"
 
 # Download bootstrap file
