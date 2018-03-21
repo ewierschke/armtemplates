ba263b767cb3bc090ba71f155d3f81a3a09f7564
Erik Wierschke <erik.wierschke@plus3it.com>
2018-03-21 11:22:21 -0400

    add control file check for preventing multiple executions

--- scripts/schedulenextscripttrim.ps1
+++ scripts/schedulenextscripttrim.ps1
@@ -1,48 +1,57 @@
-# Define System variables
-$ScheduleNextScriptDir = "${env:SystemDrive}\buildscripts\1-ScheduleNextScript"
-$nextscript = "firstrdsh"
+#Create control file to prevent multiple executions
+$ctrlfilepath = "${env:windir}\Temp\schedulenextscripttrim-control.log"
 
-# Begin Script
-# Create the ScheduleNextScript log directory
-New-Item -Path $ScheduleNextScriptDir -ItemType "directory" -Force 2>&1 > $null
+if ( -not (Test-Path $ctrlfilepath -PathType Leaf)) {
+    $text = "schedulenextscripttrim.ps1 started at: $(Get-Date)"
+    $text | Out-File -Encoding ASCII -Append -FilePath $ctrlfilepath
+    # Define System variables
+    $ScheduleNextScriptDir = "${env:SystemDrive}\buildscripts\1-ScheduleNextScript"
+    $nextscript = "firstrdsh"
 
-# Get the next script
-$Stoploop = $false
-[int]$Retrycount = "0"
-do {
-    try {
-        Invoke-Webrequest "https://raw.githubusercontent.com/ewierschke/armtemplates/runwincustdata/scripts/${nextscript}.ps1" -Outfile "${ScheduleNextScriptDir}\${nextscript}.ps1";
-        Write-Host "Downloaded next script"
-        $Stoploop = $true
-        }
-    catch {
-        if ($Retrycount -gt 3){
-            Write-Host "Could not download next script after 3 retrys."
+    # Begin Script
+    # Create the ScheduleNextScript log directory
+    New-Item -Path $ScheduleNextScriptDir -ItemType "directory" -Force 2>&1 > $null
+
+    # Get the next script
+    $Stoploop = $false
+    [int]$Retrycount = "0"
+    do {
+        try {
+            Invoke-Webrequest "https://raw.githubusercontent.com/ewierschke/armtemplates/runwincustdata/scripts/${nextscript}.ps1" -Outfile "${ScheduleNextScriptDir}\${nextscript}.ps1";
+            Write-Host "Downloaded next script"
             $Stoploop = $true
-            exit 1
-        }
-        else {
-            Write-Host "Could not download next script retrying in 30 seconds..."
-            Start-Sleep -Seconds 30
-            $Retrycount = $Retrycount + 1
+            }
+        catch {
+            if ($Retrycount -gt 3){
+                Write-Host "Could not download next script after 3 retry attempts."
+                $Stoploop = $true
+                exit 1
+            }
+            else {
+                Write-Host "Could not download next script retrying in 30 seconds..."
+                Start-Sleep -Seconds 30
+                $Retrycount = $Retrycount + 1
+            }
         }
     }
-}
-While ($Stoploop -eq $false)
+    While ($Stoploop -eq $false)
 
-#Create an atlogon scheduled task to run next script
-$taskname = "RunNextScript"
-if ($PSVersionTable.psversion.major -ge 4) {
-    $A = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass ${ScheduleNextScriptDir}\${nextscript}.ps1"
-    $T = New-ScheduledTaskTrigger -AtStartup
-    $P = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -RunLevel "Highest" -LogonType "ServiceAccount"
-    $S = New-ScheduledTaskSettingsSet
-    $D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
-    Register-ScheduledTask -TaskName $taskname -InputObject $D 2>&1 
-} else {
-    invoke-expression "& $env:systemroot\system32\schtasks.exe /create /SC ONLOGON /RL HIGHEST /NP /V1 /RU SYSTEM /F /TR `"msg * /SERVER:%computername% ${msg}`" /TN `"${taskname}`"" 2>&1
-}
+    #Create an atlogon scheduled task to run next script
+    $taskname = "RunNextScript"
+    if ($PSVersionTable.psversion.major -ge 4) {
+        $A = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass ${ScheduleNextScriptDir}\${nextscript}.ps1"
+        $T = New-ScheduledTaskTrigger -AtStartup
+        $P = New-ScheduledTaskPrincipal -UserId "NT AUTHORITY\SYSTEM" -RunLevel "Highest" -LogonType "ServiceAccount"
+        $S = New-ScheduledTaskSettingsSet
+        $D = New-ScheduledTask -Action $A -Principal $P -Trigger $T -Settings $S
+        Register-ScheduledTask -TaskName $taskname -InputObject $D 2>&1 
+    } else {
+        invoke-expression "& $env:systemroot\system32\schtasks.exe /create /SC ONLOGON /RL HIGHEST /NP /V1 /RU SYSTEM /F /TR `"msg * /SERVER:%computername% ${msg}`" /TN `"${taskname}`"" 2>&1
+    }
 
-#Sleep 5min to allow other VM extensions to finish their installs then restart
-Start-Sleep -s 300
-powershell.exe "Restart-Computer -Force -Verbose";
+    #Sleep 5min to allow other VM extensions to finish their installs then restart
+    Start-Sleep -s 300
+    $text = "schedulenextscripttrim.ps1 ended with a reboot at: $(Get-Date)"
+    $text | Out-File -Encoding ASCII -Append -FilePath $ctrlfilepath
+    powershell.exe "Restart-Computer -Force -Verbose";
+}
