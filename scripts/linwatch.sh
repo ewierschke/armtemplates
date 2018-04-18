66740dc3534c8c231a4413a9d3b876b1ad8b065e
Erik Wierschke <erik.wierschke@plus3it.com>
2018-04-18 12:56:03 -0400

    always enable firewalld

--- scripts/linwatch.sh
+++ scripts/linwatch.sh
@@ -6,12 +6,12 @@
 ) > /root/update.sh
 chmod 777 /root/update.sh
 service=firewalld
+chkconfig $service on
 if (( $(pgrep $service | wc -l) > 0 ))
 then
     echo "$service is running"
 else
     /bin/systemctl start $service
-    chkconfig $service on
 fi
 yum -y install at
 yum -y install epel-release && yum -y --enablerepo=epel install python-pip wget && pip install --upgrade pip setuptools watchmaker && watchmaker -n --log-level debug --log-dir=/var/log/watchmaker --config=/usr/lib/python2.7/site-packages/watchmaker/static/config.yaml 
